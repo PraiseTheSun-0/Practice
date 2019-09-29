7b058cc692cfc89892c8906d149ecd4bd9a3e56a
Nikita S <nikpraisethesun@gmail.com>
2019-09-29 18:56:22 +0300

    Добавлены практические задания

---
+++ Practice1/Task6/Task6.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c, D;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	D = b * b - 4 * a * c;
+	if (D < 0) {
+		cout << "Нет действительных корней уравнения" << endl;
+	}
+	else if (D == 0) {
+		cout << "x = " << -b / (2 * a) << endl;
+	}
+	else {
+		cout << "x1 = " << (-b + sqrt(D)) / (2 * a) << endl;
+		cout << "x2 = " << (-b - sqrt(D)) / (2 * a) << endl;
+	}
+}
