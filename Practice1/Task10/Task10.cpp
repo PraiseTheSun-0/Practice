89d859aa31b8dab6e5a46fb74714e0ea31a04a2c
Nikita S <nikpraisethesun@gmail.com>
2019-10-10 20:46:56 +0300

    Обновлено возведение в степень

--- Practice1/Task10/Task10.cpp
+++ Practice1/Task10/Task10.cpp
@@ -4,11 +4,25 @@ using namespace std;
 
 int main()
 {
-	int number, pow, result = 1;
+	/*int number, pow, result = 1;
 	cin >> number;
 	cin >> pow;
 	for (int i = 1; i <= pow; i++) {
 		result *= number;
 	}
-	cout << result << endl;
+	cout << result << endl;*/
+	int number, pow, result = 1, counter = 0;
+	cin >> number;
+	cin >> pow;
+	if (pow < 0)cout << "Введите неотрицательную степень" << endl;
+	else if (!pow) cout << result << endl;
+	else {
+		while (!pow % 2) {
+			counter++;
+			pow /= 2;
+		}
+		for (int i = 0, result = number; i < counter; i++) result *= result;
+		for (int k = 0; k < pow; k++)result *= number;
+		cout << result << endl;
+	}
 }
