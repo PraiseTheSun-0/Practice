b2337a67835e9fbb2dbca9b9babcde1562883630
Nikita S <nikpraisethesun@gmail.com>
2020-01-12 21:45:12 +0300

    Добавлены практические задания

---
+++ Practice1/Task28/Task28.cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include <vector>
+#include <time.h>
+
+enum Order{ASC, DESC};
+
+void BozoSort(int* a, int size, Order sort = ASC)
+{
+	bool sorted = false;
+	int toSwap1, toSwap2;
+	while (!sorted)
+	{
+		toSwap1 = rand() % size;
+		do
+		{
+			toSwap2 = rand() % size;
+		} while (toSwap2 == toSwap1);
+		std::swap(a[toSwap1], a[toSwap2]);
+		sorted = true;
+		if (sort == ASC)
+		{
+			for (int k = 0; k < size - 1; k++) {
+				if (a[k] > a[k + 1])
+				{
+					sorted = false;
+					break;
+				}
+			}
+		}
+		else 
+		{
+			for (int k = 0; k < size - 1; k++) {
+				if (a[k] < a[k + 1]) {
+					sorted = false;
+					break;
+				}
+			}
+		}
+	}
+}
+
+void BozoSort(std::vector<int> &a,int size, Order sort = ASC)
+{
+
+	bool sorted = false;
+	int toSwap1, toSwap2;
+	while (!sorted)
+	{
+		toSwap1 = rand() % size;
+		do
+		{
+			toSwap2 = rand() % size;
+		} while (toSwap2 == toSwap1);
+		std::swap(a[toSwap1], a[toSwap2]);
+		sorted = true;
+		if (sort == ASC)
+		{
+			for (int k = 0; k < size - 1; k++) {
+				if (a[k] > a[k + 1])
+				{
+					sorted = false;
+					break;
+				}
+			}
+		}
+		else
+		{
+			for (int k = 0; k < size - 1; k++) {
+				if (a[k] < a[k + 1]) {
+					sorted = false;
+					break;
+				}
+			}
+		}
+	}
+}
+
+void BozoSort(int *a, int *b, int *c, Order sort = ASC)
+{
+	int arr[] = { *a,*b,*c };
+	bool sorted = false;
+	int toSwap1, toSwap2;
+	while (!sorted)
+	{
+		toSwap1 = rand() % 3;
+		do
+		{
+			toSwap2 = rand() % 3;
+		} while (toSwap2 == toSwap1);
+		std::swap(arr[toSwap1], arr[toSwap2]);
+		sorted = true;
+		if (sort == ASC)
+		{
+			for (int k = 0; k < 2; k++) {
+				if (arr[k] > arr[k + 1])
+				{
+					sorted = false;
+					break;
+				}
+			}
+		}
+		else
+		{
+			for (int k = 0; k < 2; k++) {
+				if (arr[k] < arr[k + 1]) {
+					sorted = false;
+					break;
+				}
+			}
+		}
+	}
+	*a = arr[0], *b = arr[1], *c = arr[2];
+}
+
+int main()
+{
+	srand(time(0));
+	int x, first, second, third;
+	std::cin >> x;
+	int* arr = new int[x];
+	std::vector<int> vec;
+	for (int i = 0; i < x; i++) {
+		std::cin >> arr[i];
+		vec.push_back(arr[i]);
+	}
+	first = arr[0], second = arr[1], third = arr[2];
+	
+	std::cout << "Array: " << std::endl;
+	BozoSort(arr, x);
+	for (int i = 0; i < x; i++) std::cout << arr[i]<<' ';
+	std::cout << std::endl;
+	BozoSort(arr, x, DESC);
+	for (int i = 0; i < x; i++) std::cout << arr[i]<<' ';
+	std::cout << std::endl;
+	
+	std::cout << "Vector: " << std::endl;
+	BozoSort(vec,x);
+	for (int i = 0; i < x; i++) std::cout << vec[i] << ' ';
+	std::cout << std::endl;
+	BozoSort(vec,x, DESC);
+	for (int i = 0; i < x; i++) std::cout << vec[i] << ' ';
+	std::cout << std::endl;
+
+	std::cout << "Numbers: " << std::endl;
+	BozoSort(&first, &second, &third);
+	std::cout << first << ' '<<second << ' '<<third << ' ';
+	std::cout << std::endl;
+	BozoSort(&first, &second, &third,DESC);
+	std::cout << first << ' ' << second << ' ' << third << ' ';
+	delete[] arr;
+}
---
+++ Practice1/Task30/Task30.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+char *myStrtok(char *string, const char *delim)
+{
+	static char *start;
+	static char *end;
+	int i = 0, k = 0;
+	bool found = false;
+	static bool ended;
+	if (string != NULL)start = string;
+	else if(!ended)start = &end[1];
+	else return nullptr;
+	ended = false;
+	while (!found && start[i] != '\0')
+	{
+		k = 0;
+		while (start[i] != delim[k]) {
+			k++;
+			if (delim[k] == '\0') {
+				ended = true;
+				break;
+			}
+		}
+		if (!ended)i++;
+		else
+		{
+			found = true;
+			start = &start[i];
+		}
+	}
+	if (!found)return nullptr;
+	found = false;
+	i = 0;
+	while(!found && start[i]!='\0')
+	{
+		ended = false;
+		k = 0;
+		while(start[i]!=delim[k]){
+			k++;
+			if (delim[k] == '\0') {
+				ended = true;
+				break;
+			}
+		}
+		if (ended) {
+			ended = false;
+			i++;
+			if (start[i] == '\0')ended = true;
+		}
+		else
+		{
+			found = true;
+			end = &start[i];
+			end[0] = '\0';
+		}
+	}
+	return start;
+}
+
+int main()
+{
+	char str[200];
+	char del[20];
+	std::cin.getline(str,200);
+	std::cin.getline(del, 20);
+	
+	char *cOut = myStrtok(str, del);
+	while (cOut != NULL)
+	{
+		std::cout << cOut << std::endl;
+		cOut = myStrtok(NULL, del);
+	}
+	return 0;
+}
